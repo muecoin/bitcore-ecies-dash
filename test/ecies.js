49a20c4e117e3da9694be99150cda4e2b373646b
snogcel <ledniknoj@gmail.com>
2016-08-16 20:02:56 -0600

    update test coverage

--- test/ecies.js
+++ test/ecies.js
@@ -8,8 +8,8 @@ var PrivateKey = bitcore.PrivateKey;
 
 
 
-var aliceKey = new PrivateKey('L1Ejc5dAigm5XrM3mNptMEsNnHzS7s51YxU7J61ewGshZTKkbmzJ');
-var bobKey = new PrivateKey('KxfxrUXSMjJQcb3JgnaaA6MqsrKQ1nBSxvhuigdKRyFiEm6BZDgG');
+var aliceKey = new PrivateKey('XFKfS6jQ1ic2xonndSD2Rtvwb2GRE5XJG7q2ScBnRhSJQU5zXGD9');
+var bobKey = new PrivateKey('XHYLVEzU6S4SJHAw3qoBM4PJqNHyLLSrYM2edrcEAuzBUS55LjeA');
 
 describe('ECIES', function() {
 
@@ -53,7 +53,7 @@ describe('ECIES', function() {
     .publicKey(aliceKey.publicKey);
 
   var message = 'attack at dawn';
-  var encrypted = '0339e504d6492b082da96e11e8f039796b06cd4855c101e2492a6f10f3e056a9e712c732611c6917ab5c57a1926973bc44a1586e94a783f81d05ce72518d9b0a80e2e13c7ff7d1306583f9cc7a48def5b37fbf2d5f294f128472a6e9c78dede5f5';
+  var encrypted = '0259e3da1349903aaaf3ff0d389e8086d669a9e7ae464be5b53131b590f872d96ceccc4c78c4b0b16e45f3982e4535acda1b63edfc4ebe81fd02539c4f7d720f4f206476303796e4b0d0ae247d117355fa661710dbce76d9b97ccf731040af60b1';
   var encBuf = new Buffer(encrypted, 'hex');
 
   it('correctly encrypts a message', function() {
